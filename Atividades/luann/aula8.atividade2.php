d444d03a2a8394a80620c95396438efd3ab7898c
Luann8988 <luann.85937100@gmail.com>
2026-03-27 17:23:02 -0300

    oiiii atividades arumadas

--- Atividades/luann/aula8.atividade2.php
+++ Atividades/luann/aula8.atividade2.php
@@ -8,11 +8,19 @@ $alunos = [
     ["nome" => "Leo", "nota" => 5],
     ["nome" => "Pedro", "nota" => 4],
     ["nome" => "Jonas", "nota" => 6],
-
 ];
 
-foreach ($alunos as $aluno) {
-    $situacao =  $aluno["nota"] >= 6 ? "Aprovado" : "Reprovado";
+$soma = 0;
+$total = count($alunos);
 
+foreach ($alunos as $aluno) {
+    $situacao = $aluno["nota"] >= 6 ? "Aprovado" : "Reprovado";
+    
     echo "Aluno: {$aluno["nome"]} - Situação: {$situacao} <br>";
+    
+    $soma += $aluno["nota"];
 }
+
+$media = $soma / $total;
+
+echo "<br>Média da turma: {$media}";
--- Atividades/luann/aula8.atividade4.php
+++ Atividades/luann/aula8.atividade4.php
@@ -1,9 +1,46 @@
 <?php
 
 $produtos = [
+    [
         "nome" => "Arroz",
         "preco" => 25.90,
         "quantidade" => 10
-    ];
+    ],
+    [
+        "nome" => "Feijão",
+        "preco" => 8.50,
+        "quantidade" => 20
+    ],
+    [
+        "nome" => "Macarrão",
+        "preco" => 5.75,
+        "quantidade" => 15
+    ]
+];
+
+echo "<table border='2'>";
+echo "<tr><td>Produto</td><td>Preço</td><td>Quantidade</td><td>Total</td></tr>";
+
+$total = 0;
+
+foreach($produtos as $produto){
+    $subtotal = $produto['preco'] * $produto['quantidade'];
+    $total += $subtotal;
+
+    echo "<tr>";
+    echo "<td>{$produto['nome']}</td>";
+    echo "<td>{$produto['preco']}</td>";
+    echo "<td>{$produto['quantidade']}</td>";
+    echo "<td>{$subtotal}</td>";
+    echo "</tr>";
+}
+
+echo "<tr>";
+echo "<td colspan='3'><strong>Total Geral</strong></td>";
+echo "<td>{$total}</td>";
+echo "</tr>";
+
+echo "</table>";
+
 
     
